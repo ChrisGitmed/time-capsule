32b9700ba1f1e02fc3496e944475ef420997f6bb
Chris Gitmed <cgitmed@gmail.com>
2020-12-28 22:58:30 +0000

    Started building out the real form component; Created the form background; Created the first field (upload your files here).

--- client/components/capsule-creation-form.jsx
+++ client/components/capsule-creation-form.jsx
@@ -3,13 +3,11 @@ import React from 'react';
 export default class CapsuleCreationForm extends React.Component {
   render() {
     return (
-      <form>
-        <div>
-          Upload your files here.
+      <form className="form-container">
+        <div className="row drag-drop-field">
+          <p>Upload your files here.</p>
         </div>
-        <div>
-          <button>Upload</button>
-        </div>
+
       </form>
     );
   }
