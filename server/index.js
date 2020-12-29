4e8d874d515ec2f78719fbb5312ce7042d6fb03d
Chris Gitmed <cgitmed@gmail.com>
2020-12-29 01:04:50 +0000

    Install the aws-sdk as a dependency; Set up a basic POST route in index.js

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(staticMiddleware);
 
+app.post('/api', (req, res, next) => {
+  res.status(200).send();
+});
+
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`express server listening on port ${process.env.PORT}`);
