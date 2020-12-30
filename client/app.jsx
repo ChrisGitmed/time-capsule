254a52d75e4f571905c6fb11a93456ecd96c0ae6
Chris Gitmed <cgitmed@gmail.com>
2020-12-30 18:45:39 +0000

    Remove unused middleware component (CapsuleCreationView)

--- client/app.jsx
+++ client/app.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Navbar from './components/navbar';
-import CapsuleCreationView from './pages/capsule-creation-view';
+import CapsuleCreationForm from './components/capsule-creation-form';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="page-container">
         <Navbar />
-        <CapsuleCreationView />
+        <CapsuleCreationForm />
       </div>
     );
   }
