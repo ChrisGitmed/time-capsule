f837ebf3463470737a4c5280fa64b3c775bed5c0
Chris Gitmed <cgitmed@gmail.com>
2020-12-30 20:59:05 +0000

    Created database

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,28 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "capsules" (
+	"capsuleId" serial NOT NULL,
+	"recipient" TEXT NOT NULL,
+	"content" TEXT,
+	"sendAfter" timestamp with time zone,
+	"isDownloaded" bool,
+	"userId" int ,
+	CONSTRAINT "capsules_pk" PRIMARY KEY ("capsuleId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "users" (
+	"userId" serial ,
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+ALTER TABLE "capsules" ADD CONSTRAINT "capsules_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
