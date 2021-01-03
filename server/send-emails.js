07cd38315903753969dfa1fa14106d91df543762
Chris Gitmed <cgitmed@gmail.com>
2021-01-03 01:12:00 +0000

    Expand the db query to select all capsules where sendOn is before the current time.

--- server/send-emails.js
+++ server/send-emails.js
@@ -5,12 +5,10 @@ const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL
 });
 
-// let now = new Date();
-// now = JSON.stringify(now);
-
 const sql = `
   select (recipient, content, "sendOn")
     from capsules
+    where "sendOn" < now()
 `;
 
 db.query(sql)
@@ -18,16 +16,12 @@ db.query(sql)
     for (let i = 0; i < res.rows.length; i++) {
       /*
       const rowAsArr = res.rows[i].row.split(',');
-      console.log('rowAsArr: ', rowAsArr);
       const recipient = rowAsArr[0].substring(1);
-      console.log('recipient: ', recipient);
       const location = rowAsArr[1];
-      console.log('location: ', location);
       const time = rowAsArr[2].substring(0, rowAsArr[2].length - 1);
       console.log('time: ', time);
       */
     }
-
   })
   .catch(err => {
     throw (err);
