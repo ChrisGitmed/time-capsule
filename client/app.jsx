a4a026672013d26aeb2804ca8a0626ff83b3285f
Chris Gitmed <cgitmed@gmail.com>
2021-01-05 23:50:53 +0000

    Create a file, 'app-context.js' and put it in the lib folder; Build file according to react-context; Created context for App component including user, route, handleSignIn, and handleSignOut

--- client/app.jsx
+++ client/app.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import AppContext from './lib/app-context';
+import parseRoute from './lib/parse-route';
+import decodeToken from './lib/decode-token';
 import Navbar from './components/navbar';
 import CapsuleCreationForm from './components/capsule-creation-form';
 import SignInSignUpForm from './components/sign-in-sign-up-form';
-import parseRoute from './lib/parse-route';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      user: null,
+      isAuthorizing: true,
       route: parseRoute(window.location.hash)
     };
+    this.handleSignIn = this.handleSignIn.bind(this);
+    this.handleSignOut = this.handleSignOut.bind(this);
   }
 
   componentDidMount() {
@@ -18,15 +24,34 @@ export default class App extends React.Component {
         route: parseRoute(window.location.hash)
       });
     });
+    const token = window.localStorage.getItem('time-capsule-jwt');
+    const user = token ? decodeToken(token) : null;
+    this.setState({ user, isAuthorizing: false });
+  }
+
+  handleSignIn(result) {
+    const { user, token } = result;
+    window.localStorage.setItem('time-capsule-jwt', token);
+    this.setState({ user });
+  }
+
+  handleSignOut() {
+    window.localStorage.removeItem('time-capsule-jwt');
+    this.setState({ user: null });
   }
 
   render() {
+    const { user, route } = this.state;
+    const { handleSignIn, handleSignOut } = this;
+    const contextValue = { user, route, handleSignIn, handleSignOut };
     return (
-      <div className="page-container">
-        <Navbar />
-        <SignInSignUpForm />
-        <CapsuleCreationForm />
-      </div>
+      <AppContext.Provider value={contextValue}>
+        <div className="page-container">
+          <Navbar />
+          <SignInSignUpForm />
+          <CapsuleCreationForm />
+        </div>
+      </AppContext.Provider>
     );
   }
 }
