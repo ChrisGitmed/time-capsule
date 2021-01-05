d206a7d1b6a8eee6929b82b5ed8be6170a21b728
Chris Gitmed <cgitmed@gmail.com>
2021-01-05 07:19:35 +0000

    Typo; SignInSignOut component should be SignInSignUp

--- client/app.jsx
+++ client/app.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Navbar from './components/navbar';
 import CapsuleCreationForm from './components/capsule-creation-form';
-import SignInSignOutForm from './components/sign-in-sign-up-form';
+import SignInSignUpForm from './components/sign-in-sign-up-form';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="page-container">
         <Navbar />
-        <SignInSignOutForm />
+        <SignInSignUpForm />
         <CapsuleCreationForm />
       </div>
     );
