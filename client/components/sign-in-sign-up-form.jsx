f8fe79c96d9f1fa8ceecc0757fe7ca58534ef10e
Chris Gitmed <cgitmed@gmail.com>
2021-01-05 19:10:52 +0000

    Add style, make the new form look decent

--- client/components/sign-in-sign-up-form.jsx
+++ client/components/sign-in-sign-up-form.jsx
@@ -5,20 +5,17 @@ export default class SignInSignUpForm extends React.Component {
     return (
       <form>
         <div className="form-container">
-          <div className="row">
-            <label htmlFor="username">Username: </label>
-            <input className="input-box" type="text"/>
+          <div className="row align-center">
+            <label className="pad-right username-label" htmlFor="username">Username: </label>
+            <input className="input-box" type="text" name=""username/>
           </div>
-          <div className="row">
-            <label htmlFor="password">Password: </label>
-            <input className="input-box" type="text" />
+          <div className="row align-center">
+            <label className="pad-right password-label" htmlFor="password">Password: </label>
+            <input className="input-box" type="text" name="password"/>
           </div>
-          <div className="row">
-            <em className="">Forgot your password?</em>
-          </div>
-          <div className="row">
+          <div className="row justify-space-between">
             <button className="sign-in-button">Sign In</button>
-            <button className="sign-up-button">Sign Out</button>
+            <button className="sign-up-button">Sign Up</button>
           </div>
         </div>
       </form>
