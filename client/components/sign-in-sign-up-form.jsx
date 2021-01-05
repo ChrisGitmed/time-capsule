d0c829b88b64ae5108e88a7d931f41e1db3f27a2
Chris Gitmed <cgitmed@gmail.com>
2021-01-05 07:20:15 +0000

    Typo; SignInSignOut component should be SignInSignUp

--- client/components/sign-in-sign-up-form.jsx
+++ client/components/sign-in-sign-up-form.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class SignInSignOutForm extends React.Component {
+export default class SignInSignUpForm extends React.Component {
   render() {
     return <h1>Hello World!</h1>;
   }
