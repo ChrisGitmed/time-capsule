ca1d6849d3cc8309a0829bccf24c5e6fb30f907b
Chris Gitmed <cgitmed@gmail.com>
2021-01-05 21:34:56 +0000

    Added value props to the username and password inputs; Created state for the form to hold the username and password values; Configure the handleUsernameChange and handlePasswordChange functions to assign the current value to state.

--- client/components/sign-in-sign-up-form.jsx
+++ client/components/sign-in-sign-up-form.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 export default class SignInSignUpForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleUsernameChange = this.handleUsernameChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
@@ -13,13 +17,11 @@ export default class SignInSignUpForm extends React.Component {
   }
 
   handleUsernameChange(event) {
-  // eslint-disable-next-line no-console
-    console.log('Username changed');
+    this.setState({ username: event.target.value });
   }
 
   handlePasswordChange(event) {
-  // eslint-disable-next-line no-console
-    console.log('Password changed');
+    this.setState({ password: event.target.value });
   }
 
   render() {
@@ -29,16 +31,21 @@ export default class SignInSignUpForm extends React.Component {
       handlePasswordChange
     } = this;
 
+    const {
+      username,
+      password
+    } = this.state;
+
     return (
       <form onSubmit={handleSubmit}>
         <div className="form-container">
           <div className="row align-center">
             <label className="pad-right username-label" htmlFor="username">Username: </label>
-            <input className="input-box" type="text" name="username" onChange={handleUsernameChange} />
+            <input className="input-box" type="text" name="username" onChange={handleUsernameChange} value={username} />
           </div>
           <div className="row align-center">
             <label className="pad-right password-label" htmlFor="password">Password: </label>
-            <input className="input-box" type="text" name="password" onChange={handlePasswordChange} />
+            <input className="input-box" type="text" name="password" onChange={handlePasswordChange} value={password}/>
           </div>
           <div className="row justify-space-between">
             <button className="sign-in-button">Sign In</button>
