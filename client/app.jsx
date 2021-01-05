6df3a884252c341ef43f85f05ef72d6b2535f1a0
Chris Gitmed <cgitmed@gmail.com>
2021-01-05 22:50:27 +0000

    Added lib folder with one file, parse-route.js; Added state for route in App component; Added componentDidMount method to App component; Configured componentDidMount method to listen for hash change events on the window object, and then update state.

--- client/app.jsx
+++ client/app.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 import Navbar from './components/navbar';
 import CapsuleCreationForm from './components/capsule-creation-form';
 import SignInSignUpForm from './components/sign-in-sign-up-form';
+import { parseRoute } from './lib';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState({
+        route: parseRoute(window.location.hash)
+      });
+    });
+  }
+
   render() {
     return (
       <div className="page-container">
