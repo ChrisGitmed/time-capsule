27f8bf2ec115c6687b4222b51aa4f0dc0158f8cb
Chris Gitmed <cgitmed@gmail.com>
2021-01-05 07:14:01 +0000

    Create a file in the components folder named sign-in-sign-up-form.jsx; Create a basic React Component named SignInSignUpForm; Tweak the app.jsx file to show to new component.

--- client/app.jsx
+++ client/app.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/navbar';
 import CapsuleCreationForm from './components/capsule-creation-form';
+import SignInSignOutForm from './components/sign-in-sign-up-form';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="page-container">
         <Navbar />
+        <SignInSignOutForm />
         <CapsuleCreationForm />
       </div>
     );
