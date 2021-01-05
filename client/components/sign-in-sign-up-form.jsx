d629baa35ef57c0f21b63afec84b0aaa60481085
Chris Gitmed <cgitmed@gmail.com>
2021-01-05 08:26:49 +0000

    Expand the component into a proper form

--- client/components/sign-in-sign-up-form.jsx
+++ client/components/sign-in-sign-up-form.jsx
@@ -2,6 +2,26 @@ import React from 'react';
 
 export default class SignInSignUpForm extends React.Component {
   render() {
-    return <h1>Hello World!</h1>;
+    return (
+      <form>
+        <div className="form-container">
+          <div className="row">
+            <label htmlFor="username">Username: </label>
+            <input className="input-box" type="text"/>
+          </div>
+          <div className="row">
+            <label htmlFor="password">Password: </label>
+            <input className="input-box" type="text" />
+          </div>
+          <div className="row">
+            <em className="">Forgot your password?</em>
+          </div>
+          <div className="row">
+            <button className="sign-in-button">Sign In</button>
+            <button className="sign-up-button">Sign Out</button>
+          </div>
+        </div>
+      </form>
+    );
   }
 }
