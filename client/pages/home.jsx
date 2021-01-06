59b828866b76458a8ed629a8b15a2026c6721683
Chris Gitmed <cgitmed@gmail.com>
2021-01-06 23:11:00 +0000

    Add an onClick prop to the button with an event listener to change the window.location.hash to 'create'.

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    window.location.hash = 'create';
+  }
+
   render() {
+    const { handleClick } = this;
     return (
     <>
+      <button onClick={handleClick}>Create</button>
     </>
     );
   }
