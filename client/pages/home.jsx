6845b6051e18320b3ac51eecd539b1f5459ca6b6
Chris Gitmed <cgitmed@gmail.com>
2021-01-06 23:03:26 +0000

    Turn home function into class; Adjust renderPage() method of app to show the Home Component after sign-in or sign-up

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-export default function Home(props) {
-  return (
+export default class Home extends React.Component {
+  render() {
+    return (
     <>
     </>
-  );
+    );
+  }
 }
