ebea1a11cd65c943f9d80f2a94ccfc6168a7d969
Chris Gitmed <cgitmed@gmail.com>
2021-01-06 23:32:09 +0000

    Enhance the CapsuleList Component by adding a componentDidMount method which fetches all capsules tied to the user

--- client/components/capsule-list.jsx
+++ client/components/capsule-list.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
 
 export default class CapsuleList extends React.Component {
+
+  componentDidMount() {
+    const token = localStorage.getItem('time-capsule-jwt');
+    const req = {
+      headers: {
+        'x-access-token': token
+      }
+    };
+    fetch('api/capsules', req)
+      .then(res => res.json())
+      .then(result => {
+        // eslint-disable-next-line no-console
+        console.log(result);
+      });
+  }
+
   render() {
     return <h1>Capsule list coming soon.</h1>;
   }
