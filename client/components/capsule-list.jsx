385b20d46f73342da3c05e461de0668e866cf07e
Chris Gitmed <cgitmed@gmail.com>
2021-01-07 01:44:28 +0000

    Make individual capsules look good; Fixed date/time formatting; Adjusted padding on bottom of page container.

--- client/components/capsule-list.jsx
+++ client/components/capsule-list.jsx
@@ -26,11 +26,19 @@ export default class CapsuleList extends React.Component {
 
   render() {
     const { capsuleList } = this.state;
-    const listItems = capsuleList.map(capsule =>
-      <li className="form-container" key={capsule.capsuleId}>
-          <p>Capsule ID: {capsule.capsuleId}</p>
-      </li>
-    );
+    const listItems = capsuleList.map(capsule => {
+      const date = new Date(capsule.sendOn);
+      const sendString = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'short' }).format(date);
+      return (
+        <li className="form-container" key={capsule.capsuleId}>
+          <div className="row wrap unwrap-if-larger">
+            <p className="row pad-bottom justify-center">Capsule ID: <span className="flavor-text">{capsule.capsuleId}</span></p>
+            <p className="row pad-bottom justify-center">Recipient: <span className="flavor-text">{capsule.recipient}</span></p>
+          </div>
+          <p className="row justify-center">Send on: <span className="flavor-text">{sendString}</span></p>
+        </li>
+      );
+    });
     return <ul>{listItems}</ul>;
   }
 }
