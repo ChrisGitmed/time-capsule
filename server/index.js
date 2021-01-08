514696bb3ee762a7e23fedef7cee010af3d8e19b
Chris Gitmed <cgitmed@gmail.com>
2021-01-08 01:16:17 +0000

    Changed 'url' variable  to 'path'.

--- server/index.js
+++ server/index.js
@@ -50,8 +50,8 @@ app.get('/api/download/:capsuleId', (req, res, next) => {
   db.query(sql, params)
     .then(result => {
       const { content } = result.rows[0];
-      const url = new URL(content).pathname;
-      const key = url.substring(1);
+      const path = new URL(content).pathname;
+      const key = path.substring(1);
       const bucketParams = { Bucket: 'lfztimecapsule', Key: key };
       s3.getSignedUrlPromise('getObject', bucketParams)
         .then(url => {
