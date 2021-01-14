1f3c3afab0a01e971587b96bf756be3cd42500b4
Chris Gitmed <cgitmed@gmail.com>
2021-01-14 01:41:22 +0000

    Alter the capsule list to show an icon if the file is an image; Adjust styling.

--- client/components/capsule-list.jsx
+++ client/components/capsule-list.jsx
@@ -29,13 +29,26 @@ export default class CapsuleList extends React.Component {
     const listItems = capsuleList.map(capsule => {
       const date = new Date(capsule.sendOn);
       const sendString = new Intl.DateTimeFormat('en-US', { dateStyle: 'short', timeStyle: 'short' }).format(date);
+      const {
+        capsuleId,
+        recipient,
+        type
+      } = capsule;
+      let icon;
+      if (type === 'image/png') {
+        icon = <span className="lnr lnr-camera file-type-icon flavor-text"></span>;
+      }
+      // if type = mp4/gif/????
       return (
         <li className="form-container" key={capsule.capsuleId}>
+          <div className="row justify-center">{icon}</div>
           <div className="row wrap unwrap-if-larger">
-            <p className="row pad-bottom justify-center">Capsule ID: <span className="flavor-text">{capsule.capsuleId}</span></p>
-            <p className="row pad-bottom justify-center">Recipient: <span className="flavor-text">{capsule.recipient}</span></p>
+            <p className="row pad-bottom justify-center">Capsule ID: <span className="flavor-text">{capsuleId}</span></p>
+            <p className="row pad-bottom justify-center">Recipient: <span className="flavor-text">{recipient}</span></p>
           </div>
-          <p className="row justify-center">Send on: <span className="flavor-text">{sendString}</span></p>
+          <div className="row wrap unwrap-if-larger ">
+            <p className="row pad-bottom justify-center">Send on: <span className="flavor-text">{sendString}</span></p>
+          </div>
         </li>
       );
     });
