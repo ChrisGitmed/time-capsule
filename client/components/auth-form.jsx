f4eecb72b952402a929d7b1b02f728fe1101fb3d
Chris Gitmed <cgitmed@gmail.com>
2021-02-10 23:14:53 +0000

    Refactored the AuthForm into a function component.

--- client/components/auth-form.jsx
+++ client/components/auth-form.jsx
@@ -1,55 +1,46 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import AppContext from '../lib/app-context';
 import Redirect from './redirect';
 
-export default class AuthForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: 'DemoUser',
-      password: 'password1!A',
-      message: ''
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleUsernameChange = this.handleUsernameChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleHashChangeClick = this.handleHashChangeClick.bind(this);
-  }
+export default function AuthForm(props) {
+  const context = useContext(AppContext);
+  if (context.user) return <Redirect to="my-capsules" />;
 
-  handleSubmit(event) {
+  const [username, setUsername] = useState('DemoUser');
+  const [password, setPassword] = useState('password1!A');
+  const [message, setMessage] = useState('');
+
+  function handleSubmit(event) {
     event.preventDefault();
-    const action = this.context.route.path;
-    const { password } = this.state;
+    const action = context.route.path;
     if (password.length < 8) {
-      this.setState({ message: 'Password is too short' });
+      setMessage('Password is too short.');
     } else if (!/\d/.test(password)) {
-      this.setState({ message: 'Password needs a number.' });
+      setMessage('Passord needs a number.');
     } else if (!/[A-Z]+/.test(password)) {
-      this.setState({ message: 'Password needs a capital.' });
+      setMessage('Password needs a capital.');
     } else if (!/[\W]/.test(password)) {
-      this.setState({ message: 'Password needs a symbol.' });
+      setMessage('Password needs a symbol.');
     } else {
       const req = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(this.state)
+        body: JSON.stringify({ username, password })
       };
       fetch(`/api/auth/${action}`, req)
         .then(res => {
           if (res.status === 401) {
-            this.setState({ message: 'Invalid login.' });
+            setMessage('Invalid login.');
           }
           return res.json();
         })
         .then(result => {
           if (action === 'sign-up') {
             window.location.hash = 'sign-in';
-            this.setState({
-              message: 'success'
-            });
+            setMessage('Success.');
           }
           if (result.user && result.token) {
-            this.props.onSignIn(result);
+            props.onSignIn(result);
             window.location.hash = 'my-capsules';
           }
         })
@@ -59,80 +50,57 @@ export default class AuthForm extends React.Component {
     }
   }
 
-  handleUsernameChange(event) {
-    this.setState({ username: event.target.value });
+  function handleUsernameChange(event) {
+    setUsername(event.target.value);
   }
 
-  handlePasswordChange(event) {
-    this.setState({
-      password: event.target.value,
-      message: ''
-    });
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+    setMessage('');
   }
 
-  handleHashChangeClick(event) {
-    this.setState({
-      message: ''
-    });
+  function handleHashChangeClick(event) {
+    setMessage('');
   }
 
-  render() {
-    const {
-      handleSubmit,
-      handleUsernameChange,
-      handlePasswordChange,
-      handleHashChangeClick
-    } = this;
+  const { path } = context.route;
 
-    const {
-      username,
-      password,
-      message
-    } = this.state;
+  let button = <button className="sign-in-button" >Sign In</button>;
+  if (path === 'sign-up') {
+    button = <button className="sign-up-button" >Sign Up</button>;
+  }
 
-    const { user } = this.context;
-    const { path } = this.context.route;
+  let link = <a href="#sign-in" className="auth-link" onClick={handleHashChangeClick}>Sign-up</a>;
+  if (path === 'sign-up') {
+    link = <a href="#sign-in" className="auth-link" onClick={handleHashChangeClick}>Sign-In</a>;
+  }
 
-    if (user) return <Redirect to=""/>;
+  let hiddenText = <em className="error-text">{message}</em>;
+  if (message === 'success') {
+    hiddenText = <em className="success-text">Sign-up successful.</em>;
+  }
 
-    let button = <button className="sign-in-button" >Sign In</button>;
-    if (path === 'sign-up') {
-      button = <button className="sign-up-button" >Sign Up</button>;
-    }
-
-    let link = <a href="#sign-in" className="auth-link" onClick={handleHashChangeClick}>Sign-up</a>;
-    if (path === 'sign-up') {
-      link = <a href="#sign-in" className="auth-link" onClick={handleHashChangeClick}>Sign-In</a>;
-    }
-
-    let hiddenText = <em className="error-text">{message}</em>;
-    if (message === 'success') {
-      hiddenText = <em className="success-text">Sign-up successful.</em>;
-    }
-
-    return (
-      <form onSubmit={handleSubmit}>
-        <div className="form-container auth-form">
-          <div className="row align-center">
-            <label className="pad-right username-label" htmlFor="username">Username: </label>
-            <input required disabled className="input-box" type="text" name="username" autoComplete="username" onChange={handleUsernameChange} value={username} />
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-container auth-form">
+        <div className="row align-center">
+          <label className="pad-right username-label" htmlFor="username">Username: </label>
+          <input required disabled className="input-box" type="text" name="username" autoComplete="username" onChange={handleUsernameChange} value={username} />
+        </div>
+        <div className="row align-center">
+          <label className="pad-right password-label" htmlFor="password">Password: </label>
+          <input required disabled className="input-box" type="password" autoComplete="current-password" onChange={handlePasswordChange} value={password} />
+        </div>
+        <div className="row align-center justify-space-between wrap unwrap-if-large">
+          <div className="row placeholder justify-center">
+            {hiddenText}
           </div>
-          <div className="row align-center">
-            <label className="pad-right password-label" htmlFor="password">Password: </label>
-            <input required disabled className="input-box" type="password" autoComplete="current-password" onChange={handlePasswordChange} value={password}/>
-          </div>
-          <div className="row align-center justify-space-between wrap unwrap-if-large">
-            <div className="row placeholder justify-center">
-              {hiddenText}
-            </div>
-            <div className="row justify-flex-end align-center">
-              {link}
-              {button}
-            </div>
+          <div className="row justify-flex-end align-center">
+            {link}
+            {button}
           </div>
         </div>
-      </form>
-    );
-  }
+      </div>
+    </form>
+  );
 }
-AuthForm.contextType = AppContext;
