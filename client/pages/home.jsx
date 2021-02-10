3e559ccfb847b826c8e1c271e6b37145d4553a41
Chris Gitmed <cgitmed@gmail.com>
2021-02-10 07:59:07 +0000

    Refactored Home into a function component.

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,29 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CapsuleList from '../components/capsule-list';
 import Redirect from '../components/redirect';
 import AppContext from '../lib/app-context';
 
-export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
+export default function Home() {
+  const context = useContext(AppContext);
+  if (!context.user) return <Redirect to="sign-in" />;
 
-  handleClick(event) {
+  function handleClick(event) {
     window.location.hash = 'create';
   }
 
-  render() {
-    const { handleClick } = this;
-    if (!this.context.user) return <Redirect to="sign-in"/>;
-    return (
+  return (
     <>
       <div className="row justify-center">
         <button className="create-button big-button" onClick={handleClick}>Create a new capsule</button>
       </div>
       <CapsuleList />
     </>
-    );
-  }
+  );
 }
-Home.contextType = AppContext;
