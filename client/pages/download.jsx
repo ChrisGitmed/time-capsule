3d114388814005362ed24c6b2d37e03936fa010d
Chris Gitmed <cgitmed@gmail.com>
2021-03-23 19:08:01 +0000

    Code cleanup in DownloadPage component.

--- client/pages/download.jsx
+++ client/pages/download.jsx
@@ -6,11 +6,10 @@ export default function DownloadPage() {
     event.preventDefault();
     const fullPathArray = window.location.hash.split('/');
     const capsuleId = fullPathArray[fullPathArray.length - 1];
-    const reqString = '/api/download/' + capsuleId;
     const req = {
       mode: 'no-cors'
     };
-    fetch(reqString, req)
+    fetch(`/api/download/${capsuleId}`, req)
       .then(res => res.json())
       .then(result => {
         const { url } = result;
@@ -36,49 +35,3 @@ export default function DownloadPage() {
     </div>
   );
 }
-
-/*
-export default class DownloadPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleDownload = this.handleDownload.bind(this);
-  }
-
-  handleDownload(event) {
-    event.preventDefault();
-    const fullPathArray = window.location.hash.split('/');
-    const capsuleId = fullPathArray[fullPathArray.length - 1];
-    const reqString = '/api/download/' + capsuleId;
-    const req = {
-      mode: 'no-cors'
-    };
-    fetch(reqString, req)
-      .then(res => res.json())
-      .then(result => {
-        const { url } = result;
-        const downloadLink = document.createElement('a');
-        downloadLink.href = url;
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        downloadLink.remove();
-      })
-      .catch(err => {
-        if (err) throw err;
-      });
-  }
-
-  render() {
-    const { handleDownload } = this;
-    return (
-      <div className="form-container">
-        <div className="row justify-center pad-bottom">
-          <p>Click to start your download.</p>
-        </div>
-        <div className="row justify-center">
-          <span className="lnr lnr-download download-icon" onClick={handleDownload}></span>
-        </div>
-      </div>
-    );
-  }
-}
-*/
