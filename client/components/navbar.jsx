034afdb2e076483f5484c96dea92aee04b796ae0
Chris Gitmed <cgitmed@gmail.com>
2021-03-23 19:43:52 +0000

    Code cleanup in Navbar component

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -3,20 +3,20 @@ import AppContext from '../lib/app-context';
 
 export default function Navbar() {
   const context = useContext(AppContext);
+  const { route, handleSignOut } = context;
+  let { path } = route;
 
   function handleClick(event) {
-    const { handleSignOut } = context;
     handleSignOut();
     window.location.hash = '';
   }
 
-  let links;
-  let { path } = context.route;
+  let link;
   path = path.split('/')[0];
   if (path === 'my-capsules') {
-    links = <a href="#sign-in" onClick={handleClick}>Sign Out</a>;
+    link = <a href="#sign-in" onClick={handleClick}>Sign Out</a>;
   } else if (path === 'create' || path === 'download') {
-    links = <a href="#my-capsules">Home</a>;
+    link = <a href="#my-capsules">Home</a>;
   }
 
   return (
@@ -24,40 +24,6 @@ export default function Navbar() {
     <h1>
       Time Capsule
     </h1>
-    {links}
+    {link}
   </nav>);
 }
-/*
-export default class Navbar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(event) {
-    this.props.onSignOut();
-    window.location.hash = '';
-  }
-
-  render() {
-    let links;
-    let { path } = this.context.route;
-    path = path.split('/')[0];
-    if (path === 'my-capsules') {
-      links = <a href="#sign-in" onClick={this.handleClick}>Sign Out</a>;
-    } else if (path === 'create' || path === 'download') {
-      links = <a href="#my-capsules">Home</a>;
-    }
-    return (
-      <nav>
-        <h1>
-          Time Capsule
-        </h1>
-        {links}
-      </nav>
-    );
-  }
-
-}
-Navbar.contextType = AppContext;
-*/
