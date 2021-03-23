abb5f6ec2795cc33c9425f3e7fcd5bd767ae44e1
Chris Gitmed <cgitmed@gmail.com>
2021-03-23 18:59:33 +0000

    Refactored the DownloadPage class component to a function component.

--- client/pages/download.jsx
+++ client/pages/download.jsx
@@ -1,5 +1,43 @@
 import React from 'react';
 
+export default function DownloadPage() {
+
+  function handleDownload(event) {
+    event.preventDefault();
+    const fullPathArray = window.location.hash.split('/');
+    const capsuleId = fullPathArray[fullPathArray.length - 1];
+    const reqString = '/api/download/' + capsuleId;
+    const req = {
+      mode: 'no-cors'
+    };
+    fetch(reqString, req)
+      .then(res => res.json())
+      .then(result => {
+        const { url } = result;
+        const downloadLink = document.createElement('a');
+        downloadLink.href = url;
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        downloadLink.remove();
+      })
+      .catch(err => {
+        if (err) throw err;
+      });
+  }
+
+  return (
+    <div className="form-container">
+      <div className="row justify-center pad-bottom">
+        <p>Click to start your download.</p>
+      </div>
+      <div className="row justify-center">
+        <span className="lnr lnr-download download-icon" onClick={handleDownload}></span>
+      </div>
+    </div>
+  );
+}
+
+/*
 export default class DownloadPage extends React.Component {
   constructor(props) {
     super(props);
@@ -43,3 +81,4 @@ export default class DownloadPage extends React.Component {
     );
   }
 }
+*/
