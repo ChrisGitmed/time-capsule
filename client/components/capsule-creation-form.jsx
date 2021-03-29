29e0807c43e08a45325656a8ca3f427c44cc71ee
Chris Gitmed <cgitmed@gmail.com>
2021-03-29 22:16:17 +0000

    Refactored the CapsuleCreationForm into a function component.

--- client/components/capsule-creation-form.jsx
+++ client/components/capsule-creation-form.jsx
@@ -1,7 +1,130 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import Redirect from './redirect';
 import AppContext from '../lib/app-context';
 
+export default function CapsuleCreationForm() {
+  const [fileName, setFileName] = useState('');
+  const { user } = useContext(AppContext);
+
+  if (!user) return <Redirect to="sign-in" />;
+
+  const fileInput = React.createRef();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const token = localStorage.getItem('time-capsule-jwt');
+    const form = new FormData(event.target);
+    const date = form.get('date');
+    const time = form.get('time');
+    const dateArray = date.split('-');
+    const timeArray = time.split(':');
+    const [
+      year,
+      month,
+      day
+    ] = dateArray;
+    const [
+      hour,
+      minute
+    ] = timeArray;
+    const sendOn = JSON.stringify(new Date(year, month - 1, day, hour, minute));
+    form.append('sendOn', sendOn);
+    form.delete('time');
+    form.delete('date');
+    const req = {
+      method: 'POST',
+      headers: {
+        'x-access-token': token
+      },
+      body: form
+    };
+    fetch('/api/uploads', req)
+      .then(
+        event.target.reset(),
+        setFileName('')
+        /*
+        this.setState({
+          fileName: ''
+        })
+        */
+      )
+      .then(res => {
+        window.location.hash = 'my-capsules';
+      })
+      .catch(err => {
+        if (err) throw err;
+      });
+  }
+
+  function handleClick(event) {
+    // const fileInput = this.fileInput.current;
+    fileInput.current.click();
+  }
+
+  function handleDrop(event) {
+    event.preventDefault();
+    if (event.dataTransfer.items &&
+      event.dataTransfer.items[0].kind === 'file') {
+      fileInput.current.files = event.dataTransfer.files;
+      setFileName(fileInput.current.files[0].name);
+      /*
+      this.setState({
+        fileName: fileInput.files[0].name
+      });
+      */
+    }
+  }
+
+  function handleDragOver(event) {
+    event.preventDefault();
+  }
+
+  function handleChange(event) {
+    // const fileInput = this.fileInput.current;
+    setFileName(fileInput.current.files[0].name);
+    /*
+    this.setState({
+      fileName: fileInput.files[0].name
+    });
+    */
+  }
+
+  // if (!this.context.user) return <Redirect to="sign-in" />;
+
+  let dropZoneText = <p>Click to upload a file, or drag and drop.</p>;
+  if (fileName !== '') {
+    dropZoneText = <p className="success-text">{fileName}</p>;
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-container">
+        <div className="row drop-zone input-box" onClick={handleClick} onDrop={handleDrop} onDragOver={handleDragOver}>
+          {dropZoneText}
+        </div>
+        <input required ref={fileInput} className="hidden" type="file" name="file" onChange={handleChange} />
+        <div className="row recipient-section">
+          <label className="pad-right" htmlFor="recipient">Recipient:</label>
+          <input required className="input-box" type="email" name="recipient" placeholder="john@example.com" />
+        </div>
+        <div className="date-time-row justify-space-between unwrap-if-large">
+          <div className="date-section">
+            <label className="date-label pad-right" htmlFor="date">Date: </label>
+            <input required className="input-box" type="date" name="date" />
+          </div>
+          <div className="time-section">
+            <label className="time-label pad-right" htmlFor="time">Time: </label>
+            <input required className="input-box" type="time" name="time" />
+          </div>
+        </div>
+      </div>
+      <div className="row justify-center">
+        <button className="submit-button big-button">Seal the time capsule</button>
+      </div>
+    </form>
+  );
+
+}
+/*
 export default class CapsuleCreationForm extends React.Component {
   constructor(props) {
     super(props);
@@ -134,3 +257,4 @@ export default class CapsuleCreationForm extends React.Component {
   }
 }
 CapsuleCreationForm.contextType = AppContext;
+*/
